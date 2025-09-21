b0f32f675f2c8e84549e40b6f88f8321bd5024bf
pro804 <petsoulis.appdev@gmail.com>
2025-09-21 18:41:51 +0300

    feat: build Sidebar component structure with dynamic content - Create Sidebar component with dynamically rendered links from data.js - Map through links and social arrays to generate navigation items - Integrate custom Title component and close button with context connection - Establish foundation for CSS-driven functionality and animations

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,42 @@
+import Title from "./Title";
+import { links, social } from "../data";
+import { FaTimes } from "react-icons/fa";
+import useGlobalContext from "../hooks/useGlobalContext";
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className="side-bar">
+      <div className="sidebar-header">
+        <Title text1={"Sidebar"} text2={"modal"} />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
